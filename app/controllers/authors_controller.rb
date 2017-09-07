e7f016a585b69c60ef478ce409b3d6e26825ae51
JarfigNewton <jaredschil@gmail.com>
2017-09-07 01:13:22 +0000

    Adds Sign In/Out links to navbar. Requires authentication for all create, update, destroy actions.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  
   def new
     @author = Author.new
   end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @books = 
